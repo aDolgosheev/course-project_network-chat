cf6864f35b7f468dadf4a463c628975d2dd23b16
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2023-09-10 21:47:47 +0300

    update 2023.09.10

---
+++ src/main/java/ru/netology/dolgosheev/config/Config.java
@@ -0,0 +1,46 @@
+package ru.netology.dolgosheev.config;
+
+import ru.netology.dolgosheev.log.Log;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Config {
+
+    private static Config instance;
+
+    private static final Log LOGGER = Log.getInstance();
+    private static final String PATH = "./settings/settings.properties";
+
+    private int port;
+    private String host;
+
+    private Config() {
+        try (FileReader fileReader = new FileReader(PATH)) {
+            Properties props = new Properties();
+            props.load(fileReader);
+
+            port = Integer.parseInt(props.getProperty("port"));
+            host = props.getProperty("host");
+        } catch (IOException e) {
+            LOGGER.log("Ошибка в конструкторе у класса " + Config.class.getName());
+            e.printStackTrace();
+        }
+    }
+
+    public static Config getInstance() {
+        if (instance == null) {
+            instance = new Config();
+        }
+        return instance;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public String getHost() {
+        return host;
+    }
+}
--- src/main/java/ru/netology/dolgosheev/log/Log.java
+++ src/main/java/ru/netology/dolgosheev/log/Log.java
@@ -1,10 +1,16 @@
 package ru.netology.dolgosheev.log;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Log {
     public static Log instance = null;
-    public static final String logPath = "./log.txt";
+    public static final String LOGPATH = "./logs/log.txt";
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     private Log() {
     }
@@ -16,11 +22,13 @@ public class Log {
         return instance;
     }
 
-    public void log(String level, String msg) {
-        if (!freqs.containsKey(level)) {
-            freqs.put(level, 0);
+    public void log(String message) {
+        try {
+            FileWriter writer = new FileWriter(LOGPATH, true);
+            writer.write(LocalDateTime.now().format(formatter) + " " + message + "\n");
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        freqs.put(level, freqs.get(level) + 1);
-        System.out.println("[" + level + "#" + freqs.get(level) + "] " + LocalDateTime.now() + " === " + msg);
     }
 }
